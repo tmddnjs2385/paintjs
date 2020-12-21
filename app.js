7b250746a1e5893d13d2d6962ba0ebf07aee3239
leeseungwon <tmddnjs2385@naver.com>
2020-12-22 03:38:19 +0900

    style second

---
+++ app.js
@@ -0,0 +1,14 @@
+const canvas = document.getElementById("jsCanvas");
+
+function onMouseMove(event) {
+    console.log(event);
+}
+
+
+if (canvas) {
+    canvas.addEventListener("mousmove", onMouseMove);
+} else {
+
+
+
+}
