9ce1cc782fdd691843040e0f64b929f1fa16dcae
leeseungwon <tmddnjs2385@naver.com>
2020-12-22 04:06:34 +0900

    styles part two

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const canvas = document.getElementById("jsCanvas");
-
 function onMouseMove(event) {
     console.log(event);
 }
-
 if (canvas) {
     canvas.addEventListener("mousmove", onMouseMove);
 } else {
