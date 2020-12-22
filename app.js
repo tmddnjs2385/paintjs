d3c0fb8900d0f44dd0e95206cc8a90b80db90ea5
leeseungwon <tmddnjs2385@naver.com>
2020-12-23 01:45:43 +0900

    2D Context

--- app.js
+++ app.js
@@ -1,8 +1,71 @@
 const canvas = document.getElementById("jsCanvas");
-function onMouseMove(event) {
-    console.log(event);
+
+const ctx = canvas.getContext("2d");
+
+
+canvas.width = 700;
+
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+
+    painting = false;
+
 }
+
+function startPainting() {
+    painting = true;
+}
+
+function onMouseMove(event) {
+
+
+
+    const x = event.offsetX;
+
+    const y = event.offsetY;
+
+    console.log(x, y);
+
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function onMouseDown(event) {
+
+    painting = true;
+
+}
+
+function onMouseUp(event) {
+    stopPainting();
+
+}
+
+// function onMouseLeave(event){
+//     painting=false;
+// }
+
 if (canvas) {
-    canvas.addEventListener("mousmove", onMouseMove);
+
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+    // canvas.addEventListener("touchmove", startPainting);
+    // canvas.addEventListener("touchend", stopPainting)
+
 } else {
+
 }
